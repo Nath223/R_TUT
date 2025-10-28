b53c52c146d0d24beeb005215f323355d10654ba
Gadiel <Gadiel@DESKTOP-8ASCLJQ>
2025-10-27 18:04:02 -0600

    CAMBIOS DEL MODELO

--- Models/DOCUMENTOS.cs
+++ Models/DOCUMENTOS.cs
@@ -10,14 +10,15 @@ namespace R_TUT.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // ✅ Autoincremental
         public int Id { get; set; }
         [Required]
+        public string Usuario { get; set; } = string.Empty;
+        [Required]
+        public int Cuatrimestre { get; set; }
+        [Required]
         public string Materia { get; set; } = string.Empty;
         [Required]
         public string Titulo { get; set; } = string.Empty;
         [Required]
-        public  string Autor { get; set; } = string.Empty;
-        [Required]
         public string Descripcion { get; set; } = string.Empty;
-        [Required]
         public string Bibliografia { get; set; } = string.Empty;
         [Required]
         public required byte[] Documento { get; set; }
